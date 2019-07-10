93fe5f623ef62037d224f966275b879e02ac70f7
vinayakg709 <vinayakg709@gmail.com>
2019-07-10 10:26:39 +0530

    made gallery and team component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    GalleryComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlickCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
